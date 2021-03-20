aba3eedbb94fe15970650a1cbd8eccb3a405a95d
Onur Hüseyin Çantay <ohc3807@gmail.com>
2021-03-20 13:08:17 +0100

    Added Product structure and updated gitignorre

---
+++ ProductHunter/Architecture/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  ProductHunter
+//
+//  Created by Onur Hüseyin Çantay on 20.03.2021.
+//
+
+import Foundation
+
+// TODO: Add Generic functionality here if you see repepetitive behavior
+protocol ViewModel { }
+
+
+
+
+
---
+++ ProductHunter/Scenes/Products/ProductsView.swift
@@ -0,0 +1,12 @@
+//
+//  ProductsView.swift
+//  ProductHunter
+//
+//  Created by Onur Hüseyin Çantay on 20.03.2021.
+//
+
+import UIKit
+
+final class ProductsView: BaseView {
+  
+}
