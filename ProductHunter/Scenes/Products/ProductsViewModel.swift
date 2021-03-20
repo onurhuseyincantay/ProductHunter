cdb795158988a9f4cc53f4076e826f13e2a1a64f
Onur Hüseyin Çantay <ohc3807@gmail.com>
2021-03-20 12:53:51 +0100

    Added project structure and gitignore

---
+++ ProductHunter/Scenes/Products/ProductsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProductsViewModel.swift
+//  ProductHunter
+//
+//  Created by Onur Hüseyin Çantay on 20.03.2021.
+//
+
+import Foundation
+
+final class ProductsViewModel: ViewModel {
+  
+}
